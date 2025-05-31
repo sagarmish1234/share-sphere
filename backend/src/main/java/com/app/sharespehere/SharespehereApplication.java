9452b06c40b5db1a76f510b9c5deaade783c6c7f
sagarmish1234@gmail.com <sagarmish1234@gmail.com>
2025-05-31 09:04:12 +0530

    Backend first commit

---
+++ backend/src/main/java/com/app/sharespehere/SharespehereApplication.java
@@ -0,0 +1,13 @@
+package com.app.sharespehere;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SharespehereApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SharespehereApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/app/sharespehere/SharespehereApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.sharespehere;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SharespehereApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
