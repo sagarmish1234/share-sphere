9643360d293a5abbf27387ec95e6f79318a5dcb8
sagarmish1234@gmail.com <sagarmish1234@gmail.com>
2025-06-01 19:33:47 +0530

    local database setup terraform

---
+++ terraform-infra/local/main.tf
@@ -0,0 +1,51 @@
+terraform {
+  required_providers {
+    docker = {
+      source = "kreuzwerker/docker"
+      version = "3.6.0"
+    }
+  }
+}
+
+provider "docker" {
+  # Configuration options
+}
+
+resource "docker_network" "share-shere-local" {
+  name = "share-shere-local"
+  driver = "bridge"
+}
+
+resource "docker_image" "postgres" {
+  name = "postgres:latest"
+  keep_locally = false
+}
+
+resource "docker_volume" "postgres_data" {
+  name = "postgres-data"
+}
+
+resource "docker_container" "postgres_container" {
+  name  = "postgres-local"
+  image = docker_image.postgres.image_id
+
+  ports {
+    internal = 5432
+    external = 5432
+  }
+
+  env = [
+    "POSTGRES_USER=admin",
+    "POSTGRES_PASSWORD=admin",
+    "POSTGRES_DB=sharesphere"
+  ]
+
+  volumes {
+    volume_name = docker_volume.postgres_data.name
+    container_path = "/var/lib/postgresql/data"
+  }
+
+  restart = "unless-stopped"
+}
+
+
